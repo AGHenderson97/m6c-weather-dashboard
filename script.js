339e814433b4d3e36e021431a2213e37acf9952f
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-18 11:30:30 -0400

    working out errors

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var currentCity = "";
 
 // Function to display current weather for a city
 function displayCurrentWeather(city) {
-    // Construct the query URL
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+  // Construct the query URL
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-    // Make the API request
+  // Make the API request
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -22,10 +22,10 @@ function displayCurrentWeather(city) {
       $("#current-date").text(moment().format("MMM D, YYYY"));
       $("#current-icon").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
 
-       // Convert the temperature from Kelvin to Fahrenheit
-       var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+      // Convert the temperature from Kelvin to Fahrenheit
+      var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
-        // Display the temperature, humidity, and wind speed
+      // Display the temperature, humidity, and wind speed
       $("#current-temp").text(tempF.toFixed(1) + " °F");
       $("#current-humidity").text(response.main.humidity + "%");
       $("#current-wind").text(response.wind.speed + " MPH");
@@ -43,19 +43,15 @@ function displayCurrentWeather(city) {
           $("#current-uv").text(uvResponse.value);
 
           // Set the UV index background color based on its value
-          if (uvResponse.value < 3) {
+          if (uvResponse.value <3) {
             $("#current-uv").css("background-color", "green");
-          }
-          else if (uvResponse.value < 6) {
+          } else if (uvResponse.value <6) {
             $("#current-uv").css("background-color", "yellow");
-          }
-          else if (uvResponse.value < 8) {
+          } else if (uvResponse.value <8) {
             $("#current-uv").css("background-color", "orange");
-          }
-          else if (uvResponse.value < 11) {
+          } else if (uvResponse.value <11) {
             $("#current-uv").css("background-color", "red");
-          }
-          else {
+          } else {
             $("#current-uv").css("background-color", "purple");
           }
         }
@@ -69,10 +65,10 @@ function displayCurrentWeather(city) {
 
 // Function to display the 5-day forecast for a city
 function displayForecast(city) {
-    // Construct the query URL
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+  // Construct the query URL
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
-     // Make the API request
+  // Make the API request
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -80,6 +76,6 @@ function displayForecast(city) {
     success: function(response) {
       // Display the forecast cards
       for (var i = 0; i < response.list.length; i += 8) {
-      // Calculate the date, icon, temperature, and humidity for the forecast card
-      var date = moment(response.list[i].dt_txt).format("MMM D, YYYY");
-      var icon = "https://openweathermap.org/img/w/" + response.list}
+        // Calculate the date, icon, temperature, and humidity for the forecast card
+        var date = moment(response.list[i].dt_txt).format("MMM D, YYYY");
+        var icon = "https

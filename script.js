67f9ea70b022b34b703abc8cf795a1f36fed5674
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-18 10:50:26 -0400

    function, current weather display

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 var apiKey = "YOUR_API_KEY_HERE";
 var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 var currentCity = "";
+
+// Function to display current weather for a city
+function displayCurrentWeather(city) {
+    // Construct the query URL
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
